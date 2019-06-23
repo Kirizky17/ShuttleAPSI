91bb55ffebd65bfca1b85778c4de0a737ac5ac99
Rizky <rizkywahyudi175@gmail.com>
2019-06-23 20:07:15 +0700

    Second Commit

---
+++ resources/views/nonmember.blade.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+	<div class="judul">
+		<center><strong><h1>PEMESANAN KURSI</h1></strong></center>
+	</div>
+	<div class="content" style="margin-left: 420px">
+		<form action="" method="post">
+		<table style="margin:20px auto;padding-left: 100px;float: left;">
+		<tr>
+				<td>Nama : </td>
+				<td><input type="text" name="nama" class="form-control" style="padding-right:73px;padding-top: 5px;padding-bottom: 5px;margin-top: 10px " ></td>
+		</tr>
+		<tr>
+				<td>No Telepon : </td>
+				<td><input type="text" name="nama" class="form-control" style="padding-right:73px;padding-top: 5px;padding-bottom: 5px;margin-top: 10px " ></td>
+		</tr>
+		<tr>
+			<td></td>
+			<td><center><input type="submit" value="Cari" class="btn btn-primary"></center></td>
+		</tr>
+	</table>
+	</form>
+	</div>
+</body>
+</html>
---
+++ resources/views/pemilihankursi.blade.php
@@ -0,0 +1,75 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+	<div class="judul">
+		<center><strong><h1>PEMESANAN KURSI</h1></strong></center>
+	</div>
+	<div class="content">
+	<a href="#" style="padding-left: 100px"><button>Kembali</button></a>
+		<form action="" method="post">
+		
+ 
+	<table style="margin:20px auto;padding-left: 100px;float: left;">
+			<tr>
+				<td>Agen:</td>
+				<td><select name="asal" class="form-control" style="padding-right:72px;padding-top: 5px;padding-bottom: 5px">
+					<option value ="null">Please select value...</option>
+				</select></td>
+				<td></td>
+				<td></td>
+				<td style="padding-left: 400px">Tanggal Keberangkatan : </td>
+				<td><input type="date" name="tanggalkeberangkatan" class="form-control" style="padding-right:100px;padding-top: 5px;padding-bottom: 5px;margin-left: 10px" >
+				</td>
+			</tr>
+			<tr>
+				<td>Harga : </td>
+				<td><input type="text" name="tanggalkeberangkatan" class="form-control" style="padding-right:73px;padding-top: 5px;padding-bottom: 5px;margin-top: 10px " ></td>
+				<td></td>
+				<td></td>
+				<td style="padding-left: 400px">Waktu Keberangkatan : </td>
+				<td><input type="date" name="waktukeberangkatan" class="form-control" style="padding-right:100px;padding-top: 5px;padding-bottom: 5px;margin-left: 10px" ></td>
+			</tr>
+			<tr>
+				<td>Tempat Naik : </td>
+				<td><select name="tempatnaik" class="form-control" style="padding-right:72px;padding-top: 5px;padding-bottom: 5px;margin-top: 10px">
+					<option value ="null">Please select value...</option>
+				</select></td>
+				<td></td>
+				<td></td>
+				
+			</tr>
+			<tr>
+				<td>Tempat Turun:</td>
+				<td><select name="tempatturun" class="form-control" style="padding-right:72px;padding-top: 5px;padding-bottom: 5px;margin-top: 10px">
+					<option value ="null">Please select value...</option>
+				</select></td>
+				<td></td>
+				<td></td>
+				
+			</tr>
+			<tr>
+				<td></td>
+				<td></td>
+				<td></td>
+				<td></td>
+				<td></td>
+				
+			</tr>
+		</table>
+		<table style="margin:20px auto;padding-left: 100px;float: left;border: 1px;margin-left: 250px;">
+		<tr>
+			<th>Kode Kursi</th>
+			<th style="padding-left: 500px">Status</th>
+		</tr>
+		<tr>
+			<td><center>---kursi---</center></td>
+			<td style="padding-left: 500px"><center>---status---</center></td>
+		</tr>
+		</table>
+	</form>
+</div>
+</body>
+</html>
