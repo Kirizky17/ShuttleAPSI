dbf55c8e166ba7994a2f5502cebb9886f9b2de31
Rizky <rizkywahyudi175@gmail.com>
2019-06-24 14:54:55 +0700

    Fourth Commit

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,4 +1,19 @@
 @extends('template')
 @section('content')
-HOME
+<div class="row">
+    <div id="homepage-title">
+        <h1 class="text-center">SELAMAT DATANG DI <b>SHUTTLE.CO</b></h1>
+    </div>
+</div>
+<div class="row">
+    <div class="col-lg-4 col-md-4"></div>
+    <div class="col-lg-4 col-md-4" id="menu-bar">
+        <ul class="nav navbar-nav">
+            <li><a href="/Pesan">PEMESANAN TIKET</a></li>
+            <li><a href="/Beli">PEMBELIAN TIKET</a></li>
+            <li><a href="/Batal">PEMBATALAN TIKET</a></li>
+        </ul>
+    </div>
+    <div class="col-lg-4 col-md-4"></div>
+</div>
 @endsection
