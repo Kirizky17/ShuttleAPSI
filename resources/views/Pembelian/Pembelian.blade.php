314b09c12f8fcb364758f3753f52a2ded404de6e
Sri Meliani <smeliani17@gmail.com>
2019-06-24 17:23:01 +0700

    submit pembelian

--- resources/views/Pembelian/Pembelian.blade.php
+++ resources/views/Pembelian/Pembelian.blade.php
@@ -5,9 +5,16 @@
     <div id="page-title" align="center">
         <h1 class="text-center" style="margin: 20px 0 !important;">Masukkan Kode Pemesanan</h1>
         <form action="" method="post">
+        	<div class="row little-margin-ver">
         	<div class="col-md-4 col-lg-4"></div>
         	<div class="col-md-4 col-lg-4">
         		<input name="kode" class="form-control">
+        	</div>
+        	<div class="col-md-4 col-lg-4"></div>
+        	</div>
+        	<div class="col-md-4 col-lg-4"></div>
+        	<div class="col-md-4 col-lg-4">
+        		<input type="submit" value="Submit" class="btn btn-primary" style="float:center;">
         	</div>
         	<div class="col-md-4 col-lg-4"></div>
         </form>
