771ee9f79916da39c373c5aa86c454f2e139a753
Faza <fazakbar76@gmail.com>
2019-06-25 09:04:04 +0700

    resolve web.php

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -16,4 +16,43 @@
     </div>
     <div class="col-lg-4 col-md-4"></div>
 </div>
+
+<div style="width:250px;height:500px;border:2px solid #000;background-color: white;"align="center" id="printableArea">
+    <div align="center">
+        <img src ="https://www.bcgen.com/demo/linear-dbgs.aspx?D=<?php echo "123123123"
+        ?>"><br><br><br>
+
+              <!--Ticket Information-->
+              <h3> SHUTTLE.CO</h3>
+              <br><br><br>
+              <h4>Waktu Keberangkatan :</h4>
+              <br>
+              <h4>Saturday August 17, 2019 1:00PM</h4>
+              Seat: <?php echo "Nomor Kursi"?>
+              <br>
+
+        <img src="/images/flat5.jpg" width="200px" height="180px">
+    </div>
+</div>
+
+<br>
+  <form action = "pullticket.html" method= "post">
+  <input type="button" onclick="printDiv('printableArea')" value="Cetak Tiket"/>
+  </form>
+
+
 @endsection
+
+
+<script type="text/javascript">
+function printDiv(divName) {
+   var printContents = document.getElementById(divName).innerHTML;
+   var originalContents = document.body.innerHTML;
+
+   document.body.innerHTML = printContents;
+
+   window.print();
+
+   document.body.innerHTML = originalContents;
+}
+</script>
